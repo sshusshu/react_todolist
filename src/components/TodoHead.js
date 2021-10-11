b0176c01505ba2a72b198ae0f333de770070d70e
subin <subin9033@gmail.com>
2021-10-11 16:35:34 +0900

    CHORE : build todolist

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useTodoState } from "../TodoContext";
 
 const TodoHeadBlock = styled.div`
   padding-top: 48px;
@@ -25,11 +26,24 @@ const TodoHeadBlock = styled.div`
 `;
 
 export const TodoHead = () => {
+  const todos = useTodoState();
+  const undoneTasks = todos.filter((todo) => !todo.done);
+  const today = new Date();
+  const dateString = today.toLocaleDateString(
+    ("ko-KR",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    })
+  );
+
+  const dayName = today.toLocaleDateString("ko-KR", { weekday: "long" });
   return (
     <TodoHeadBlock>
-      <h1>2021년 10월 11일</h1>
-      <div className="day">월요일</div>
-      <div className="tasks-left">할 일 2개 남음</div>
+      <h1>{dateString}</h1>
+      <div className="day">{dayName}</div>
+      <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
     </TodoHeadBlock>
   );
 };
