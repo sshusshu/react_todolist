7abc1a631ecea8dfd68df8417d0a126328c0fcbb
subin <subin9033@gmail.com>
2021-10-11 10:16:11 +0900

    DELETE : delete everything

--- src/App.js
+++ src/App.js
@@ -1,27 +1,5 @@
-import { createGlobalStyle } from "styled-components";
-import TodoCreate from "./components/TodoCreate";
-import TodoHead from "./components/TodoHead";
-import TodoList from "./components/TodoList";
-import TodoTemplate from "./components/TodoTemplate";
-import { TodoProvider } from "./TodoContext";
-
-const GlobalStyle = createGlobalStyle`
-body{
-  background:#e9ecef
-}
-`;
-
 function App() {
-  return (
-    <TodoProvider>
-      <GlobalStyle />
-      <TodoTemplate>
-        <TodoHead />
-        <TodoList />
-        <TodoCreate />
-      </TodoTemplate>
-    </TodoProvider>
-  );
+  return <></>;
 }
 
 export default App;
